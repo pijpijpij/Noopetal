61fe764ca4bed97de9ef5f8aa9cb88768631c0f4
pijpijpij <atlassian@champault.co.uk>
2015-10-29 18:25:57 +0000

    Fixed error logging when an unexpected exception occurs during generation.

--- noopetal-compiler/src/main/java/com/pij/noopetal/NoopetalProcessor.java
+++ noopetal-compiler/src/main/java/com/pij/noopetal/NoopetalProcessor.java
@@ -184,7 +184,7 @@ public final class NoopetalProcessor extends AbstractProcessor {
     private void logParsingError(Element element, Class<? extends Annotation> annotation, Exception e) {
         StringWriter stackTrace = new StringWriter();
         e.printStackTrace(new PrintWriter(stackTrace));
-        error(element, "Unable to parse @%s binding.\n\n%s", annotation.getSimpleName(), stackTrace);
+        error(element, "Unable to parse @%s annotation.\n\n%s", annotation.getSimpleName(), stackTrace);
     }
 
     /**
